12ca26e28ae5b74e035910846ff968b74628df3e
LABB <contact@labb.email>
2024-02-16 16:25:50 -0300

    [MAIN] Extract common test logic.

---
+++ src/Abstracts/RouteTestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SnakeInk\Crude\Abstracts;
+
+use SnakeInk\Crude\Concerns\CreatesApplication;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithExceptionHandling;
+
+abstract class RouteTestCase extends BaseTestCase
+{
+    use InteractsWithExceptionHandling;
+    use InteractsWithDatabase;
+    use CreatesApplication;
+}
