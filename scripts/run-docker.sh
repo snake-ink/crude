fbf1e741ba92449f0b2808ffa244672d0a8760a3
LABB <contact@labb.email>
2024-02-16 16:33:48 -0300

    [MAIN]
    * Atualiza route-test.stub;
    * Extrai lógica de testes para lib.

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -4,5 +4,5 @@ docker run --rm \
     -v "$(pwd):/opt:z" \
     -v "$(pwd)/docker/development/ssh:/root/.ssh:z" \
     -w /opt \
-    laravelsail/php82-composer:latest \
+    laravelsail/php83-composer:latest \
     "$@"
--- src/Abstracts/RouteTestCase.php
+++ src/Abstracts/RouteTestCase.php
@@ -2,6 +2,7 @@
 
 namespace SnakeInk\Crude\Abstracts;
 
+use Illuminate\Auth\GenericUser;
 use SnakeInk\Crude\Concerns\CreatesApplication;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
@@ -12,4 +13,13 @@ abstract class RouteTestCase extends BaseTestCase
     use InteractsWithExceptionHandling;
     use InteractsWithDatabase;
     use CreatesApplication;
+
+    protected GenericUser $genericUser;
+
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+
+        $this->genericUser = new GenericUser(['id' => 1, 'name' => 'Surucucu']);
+    }
 }
