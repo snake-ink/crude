bb82bdb4c6895c0bfef6760bab8dbee5faef0ed0
LABB <contact@labb.email>
2024-02-16 17:15:38 -0300

    [MAIN]
    * Adiciona método generateDefaultUserActor em RouteTestCase.

--- src/Abstracts/RouteTestCase.php
+++ src/Abstracts/RouteTestCase.php
@@ -3,6 +3,7 @@
 namespace SnakeInk\Crude\Abstracts;
 
 use Illuminate\Auth\GenericUser;
+use Illuminate\Contracts\Auth\Authenticatable;
 use SnakeInk\Crude\Concerns\CreatesApplication;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
@@ -14,12 +15,17 @@ abstract class RouteTestCase extends BaseTestCase
     use InteractsWithDatabase;
     use CreatesApplication;
 
-    protected GenericUser $genericUser;
+    protected GenericUser|Authenticatable $defaultUserActor;
 
     public function __construct(string $name)
     {
         parent::__construct($name);
 
-        $this->genericUser = new GenericUser(['id' => 1, 'name' => 'Surucucu']);
+        $this->defaultUserActor = $this->generateDefaultUserActor();
+    }
+
+    protected function generateDefaultUserActor(): GenericUser|Authenticatable
+    {
+        return new GenericUser(['id' => 1, 'name' => 'Surucucu']);
     }
 }
